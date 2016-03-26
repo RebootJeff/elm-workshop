8d2029108d106f9ee804e22ac920d3379240b45b
Jeffrey Lee <jlee@zignallabs.com>
2016-03-26 13:09:01 -0700

    Attempt exercises 1 and 2

--- stages/1/Main.elm
+++ stages/1/Main.elm
@@ -12,22 +12,26 @@ model =
       }
   }
 
+githubUrl path =
+  "https://github.com/" ++ path
 
 view model =
   div
     [ class "content" ]
     [ header
         []
-        [ -- TODO add the equivalent of <h1>ElmHub</h1> right before the tagline
-          span [ class "tagline" ] [ text "“Like GitHub, but for Elm things.”" ]
+        -- TODO add the equivalent of <h1>ElmHub</h1> right before the tagline
+        [ h1 [] [ text "ElmHub" ]
+        , span [ class "tagline" ] [ text "“Like GitHub, but for Elm things.”" ]
         ]
     , ul
         [ class "results" ]
         [ li
             []
-            [ span [ class "star-count" ] [{- TODO display the number of stars -}]
+            [ span [ class "star-count" ] [ text (toString model.result.stars) ]
               -- TODO use the model to put a link here that points to
               -- https://github.com/TheSeamau5/elm-checkerboardgrid-tutorial
+            , a [ href (githubUrl model.result.name) ] [ text "GitHub repo link" ]
             ]
         ]
     ]
